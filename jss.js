88f346bd3236c6c1be41a618156d1c19c92ffde7
CarmenBeshara <65073274+CarmenBeshara@users.noreply.github.com>
2020-06-08 04:26:38 +0200

    Add files via upload

---
+++ jss.js
@@ -0,0 +1,42 @@
+/* const countries = ['jlan', 'deenen',  'Jenmark', 'nelly'];
+
+
+  for (let i=0; i<countries.length; i++) {
+    if ( countries[i].startsWith("J") ||countries[i].startsWith("j")) {
+    
+    console.log("Goodbye " + countries[i]);
+    document.write("Goodbye " + countries[i] + "<br/> ");
+    }
+    else
+    {
+      console.log("hello " + countries[i]);
+      document.write("hello " + countries[i] + "<br/> ");
+    }
+  }
+  */
+
+
+  
+ <script>
+ function myFunction() {
+   location.replace("carmen.html")
+ }
+ </script>
+ 
+ 
+
+
+<script>
+ function myFunction2() {
+   location.replace("beshara.html")
+ }
+
+ 
+ </script>
+ 
+ <script>
+   function myFunction3() {
+     document.getElementById("F").innerHTML = "CLICKED ME!";
+   }
+   </script>
+
